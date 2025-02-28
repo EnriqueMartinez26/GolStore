0812b4509c681658d408beb2f9018942244111de
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2025-02-28 06:25:14 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './pages/Home';
