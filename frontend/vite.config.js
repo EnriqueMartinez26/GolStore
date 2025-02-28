846d86662ef49c67649fbea708671169c29faccf
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2025-02-28 07:33:12 -0300

    123

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/dist/', // Alinea las rutas con el Directorio Raíz de Render
+  base: './dist'
 });
