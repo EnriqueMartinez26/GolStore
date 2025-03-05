8631479794f2615174ec2580185251d13f23a99b
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2025-03-05 18:09:56 -0300

    123

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ async function connectDB() {
   if (isConnected) return;
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     isConnected = conn.connections[0].readyState;
