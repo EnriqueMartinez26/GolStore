74b0a6492d992c3f5258ae5d7270e778f6e3f2eb
Enrique Leonel Martínez <emartinez.03@hotmail.com>
2025-03-05 17:07:24 -0300

    123

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Cache para la conexión a la base de datos
 let isConnected;
 
 async function connectDB() {
@@ -27,7 +26,6 @@ async function connectDB() {
   }
 }
 
-// Middleware para asegurarse de que la DB esté conectada antes de cada petición
 app.use(async (req, res, next) => {
   try {
     await connectDB();
@@ -37,7 +35,7 @@ app.use(async (req, res, next) => {
   }
 });
 
-// Rutas
+
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
 const cartRoutes = require('./routes/cart');
@@ -48,12 +46,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/favorites', favoriteRoutes);
 
-// Endpoint de salud para pruebas
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-// Solo llama a app.listen() en desarrollo
 if (!process.env.VERCEL_ENV) {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
